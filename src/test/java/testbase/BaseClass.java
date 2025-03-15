6d44813aa8fa0c049f865575ea4bc6fb315795e1
kittykawaii <phamdieulyy2309@gmail.com>
2025-03-15 17:51:58 +0700

    up

--- src/test/java/testbase/BaseClass.java
+++ src/test/java/testbase/BaseClass.java
@@ -5,34 +5,50 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class BaseClass {
 
     public WebDriver driver;
     public Logger logger;
+    public Properties p;
 
     @BeforeClass
-    public void setup(){
-        logger= (Logger) LogManager.getLogger(this.getClass());
+    @Parameters({"os","browser"})
+    public void setup(String os, String br) throws IOException {
 
+        //Loading config.properties file
+        FileReader file = new FileReader("./src/test/resources/config.properties");
+        p= new Properties();
+        p.load(file);
+
+        logger= (Logger) LogManager.getLogger(this.getClass());
+        switch (br.toLowerCase()){
+            case "chrome": driver=new ChromeDriver(); break;
+            case "edge": driver=new EdgeDriver(); break;
+            case "firefox": driver=new FirefoxDriver(); break;
+            default:
+                System.out.println("Invalid browser name"); return;
+        }
         driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
-        //driver.get("https://demo.opencart.com/");
-        driver.get("http://58.186.64.252:4090");
+        driver.get(p.getProperty("appURL1")); //reading url from properties file
         driver.manage().window().maximize();
     }
-
     @AfterClass
     public void tearDown(){
         driver.quit();
     }
-
     public String randomString(){
         String generatedString = RandomStringUtils.randomAlphabetic(5);
         return generatedString;
